ed19d0fb62c0b0fa6e77709f30ea7a15079e025f
Antonio Alvarez <tokyo.blue.tsubasa@gmail.com>
2015-06-18 13:34:24 +0200

    Inicializando

---
+++ nuevaCategoria.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Nueva Categoria</title>
+</head>
+<body>
+	<div class="container">
+		
+	</div>	
+</body>
+</html>
---
+++ nuevaGaleria.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Crear Galeria</title>
+</head>
+<body>
+	
+</body>
+</html>
